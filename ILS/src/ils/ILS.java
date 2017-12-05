608c207a5562385e979b7d8c0c8362f1bac7e5df
mzboito <mzboito@gmail.com>
2017-12-05 21:55:14 -0200

    file reader function ready and returning a filled problem

--- ILS/src/ils/ILS.java
+++ ILS/src/ils/ILS.java
@@ -5,6 +5,8 @@
  */
 package ils;
 
+import java.io.IOException;
+
 /**
  *
  * @author mzboito
@@ -14,8 +16,9 @@ public class ILS {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        Problem p = new ProblemFileReader().read_file("/home/mzboito/Downloads/instances/a8.oplib");
     }
     
 }
